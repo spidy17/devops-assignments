5ad5cfe5ab2ae088aa05082790d4103234344cc6
Rohit Iparkar <iparkarrohit17@gmail.com>
2023-01-10 20:47:29 +0530

    second commit

--- rohit_assignment.sh
+++ rohit_assignment.sh
@@ -1,15 +1,18 @@
 #!bin/bash
-
+touch employeelist.txt
 for ((i=0; i<5; i++))
 do 
+  s=$(date)
   echo "enter you name"
   read name
  
-  echo "enter you name"
-  read name
+  echo "enter you nage"
+  read age
 
-  echo "enter you name"
-  read name
+  echo "enter you address"
+  read address
+  
+  echo "$name $age $address $s" >> employeelist.txt
 
   mkdir $name
   cd $name
